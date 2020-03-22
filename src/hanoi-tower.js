dd4b859e8f63ee161d39059956b84fd504b02568
dajiquwe <60528148+dajiquwe@users.noreply.github.com>
2020-03-22 23:09:11 +0300

    Update hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,5 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     const turns = 2 ** disksNumber - 1;
-    const seconds = turns / (turnsSpeed / 3600);
-    return { turns, seconds };
+    const sec = turns / (turnsSpeed / 3600);
+    return { turns, sec };
 }
